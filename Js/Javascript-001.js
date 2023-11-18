00303c2bf9a1cb60f528d1048a0c0f11480d813c
amri <amrioperatorpro@gmail.com>
2023-11-18 11:58:45 +0700

    replace: menganti kode js

--- Js/Javascript-001.js
+++ Js/Javascript-001.js
@@ -1,7 +1,17 @@
-console.log("hello js");
+/* console.log("hello js");
 
 
 kata  = "hallo";
 kata2 = "friend";
 
-console.log(kata + kata2);
+console.log(kata + kata2); */
+
+
+console.log("hallo everyone")
+
+x = true;
+y = false;
+
+z = x + y;
+
+console.log(z);
