d6a02b13538b87f28880049e993860988b4e5c22
amri <121174751+nature-modren@users.noreply.github.com>
2023-12-29 18:16:02 +0700

    retext: memperbarui tampilan

--- Js/konsep-class.js
+++ Js/konsep-class.js
@@ -1,26 +1,22 @@
-//konsep class pada javascript 
+//konsep class pada javascript
 
-class user{
-constructor(nama,umur,alamat,hobi){
-    this.nama   = nama,
-    this.umur   = umur,
-    this.alamat = alamat
-    this.hobi   = hobi
-};
-greet(){
-    return `halo ${this.nama}`
+class user {
+    constructor(nama, umur, alamat, hobi) {
+        (this.nama = nama), (this.umur = umur), (this.alamat = alamat);
+        this.hobi = hobi;
+    }
+    greet() {
+        return `halo ${this.nama}`;
+    }
 }
-};
 
 database = [
+    (user1 = new user("Amri", 28, "jl.bantan", ["musik", "ngoding"])),
 
-user1 = new user("Amri",28,"jl.bantan",["musik","ngoding"]),
+    (user2 = new user("budi", 27, "jl.pramuka", ["anime", "ngambar"])),
 
-user2 = new user("budi",27,"jl.pramuka",["anime","ngambar"]),
-
-user3 = new user("wahyu",19,"jl.soedirman", ["lukis"])
-
+    (user3 = new user("wahyu", 19, "jl.soedirman", ["lukis"]))
 ];
 
-console.table(database)
-console.log(database)
+console.table(database);
+console.log(database);
