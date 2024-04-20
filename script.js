31b461526bb5232828c960f30698be2562ab1699
nature-modren <121174751+nature-modren@users.noreply.github.com>
2024-04-20 20:58:42 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function showWelcomeMessage() {
+  document.getElementById("welcome-message").innerHTML =
+    "Welcome to our new guest!";
+}
+
+const fungsi = "amri";
+
+if (fungsi == "amri firdaus ") {
+  console.info("benar sekali");
+} else {
+  console.info("salah nama nya bukan mikro");
+}
